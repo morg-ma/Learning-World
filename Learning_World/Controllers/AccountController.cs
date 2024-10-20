fa0c079ee445b43f1e7a5b4199156e68362b0f38
Mohamed Atef <modya4796@gmail.com>
2024-10-20 19:39:23 +0300

    last

--- Learning_World/Controllers/AccountController.cs
+++ Learning_World/Controllers/AccountController.cs
@@ -1,26 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Learning_World.Models;
+using Learning_World.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Learning_World.Controllers
 {
     public class AccountController : Controller
     {
-        public AccountController()
+        private readonly UserManager<ApplicationUser> _UserManager;
+        private readonly SignInManager<ApplicationUser> _SignInManager;
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
-            
+            _UserManager = userManager;
+            _SignInManager = signInManager;
         }
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
         //[HttpGet]
         //public IActionResult Registration()
         //{
         //    return View("Registration");
         //}
         //[HttpPost]
-        //public IActionResult Registration()
+        //public async Task<IActionResult> Registration(RegistrationViewModel userViewModel)
         //{
-        //    return RedirectToAction();
+        //    if (ModelState.IsValid)
+        //    {
+        //        ApplicationUser applicationUser = new ApplicationUser();
+        //        applicationUser.UserName = userViewModel.UserName;
+        //        applicationUser.PasswordHash = userViewModel.Password;
+        //        applicationUser.Email = userViewModel.Email;
+        //        IdentityResult identityResult = await _UserManager.CreateAsync(applicationUser, userViewModel.Password);
+        //        if (identityResult.Succeeded)
+        //        {
+        //            // add Cookies
+        //            await _SignInManager.SignInAsync(applicationUser, false);
+        //            return RedirectToAction("GetAll", "Instructor");
+        //        }
+        //        foreach (var item in identityResult.Errors)
+        //        {
+        //            ModelState.AddModelError("", item.Description);
+        //        }
+        //    }
+        //    return View("Registration", userViewModel);
         //}
         //[HttpGet]
         //public IActionResult Login()
@@ -28,13 +53,30 @@ namespace Learning_World.Controllers
         //    return View();
         //}
         //[HttpPost]
-        //public IActionResult Login()
+        //public async Task<IActionResult> SaveLogin(LoginViewModel loginUserViewModel)
         //{
-        //    return RedirectToAction();
+        //    if (ModelState.IsValid)
+        //    {
+        //        ApplicationUser user = await _UserManager.FindByNameAsync(loginUserViewModel.Name);
+        //        if (user != null)
+        //        {
+        //            bool found = await _UserManager.CheckPasswordAsync(user, loginUserViewModel.Password);
+        //            if (found)
+        //            {
+        //                List<Claim> claims = new List<Claim>();
+        //                claims.Add(new Claim("Image", user.Image));
+        //                await _SignInManager.SignInWithClaimsAsync(user, loginUserViewModel.Remember, claims);
+        //                return RedirectToAction("GetAll", "Instructor");
+        //            }
+        //        }
+        //        ModelState.AddModelError("", "UserName Or Password Wrong");
+        //    }
+        //    return View("Login", loginUserViewModel);
         //}
-        //public IActionResult LogOut()
+        //public async Task<IActionResult> SignOutAsync()
         //{
-        //    return RedirectToAction();
+        //    await _SignInManager.SignOutAsync();
+        //    return RedirectToAction("Login");
         //}
     }
 }
